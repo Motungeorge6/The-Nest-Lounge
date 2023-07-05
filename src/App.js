179910ffe233931e5ef6a15f6bce18eb4f03951c
Idowu Motunrayo <motungeorge6@gmail.com>
2023-07-05 23:22:46 +0100

    Commit Menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/home/Home";
-import About from "./components/about/About";
+import Menu from "./pages/menu/Menu";
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <Menu />
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/" element={<About />} />
+    // </Routes>
   );
 };
 
---
+++ src/pages/menu/Menu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "../menu/menu.css";
+
+const Menu = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="logo-container"></div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </div>
+      <div className="menu-container"></div>
+    </div>
+  );
+};
+
+export default Menu;
