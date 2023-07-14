2f73e864f792a4e099560e428ca58dfe0f0ab086
Ben Billion <107368386+Udegbunamonyedikabenjamin@users.noreply.github.com>
2023-07-14 22:44:11 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/about/About";
 import Reserve from "./pages/reserve/Reserve";
 import Menu from "./pages/menu/Menu";
 import Menucat from "./pages/menu/Menucat";
+import Itemslist from "./pages/menu/Itemslist";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/Menu" element={<Menu />} />
         <Route path="/Menucat" element={<Menucat />} />
         <Route path="/reserve" element={<Reserve />} />
+        <Route path="/Itemslist" element={<Itemslist />} />
       </Routes>
     </>
   );
--- src/pages/menu/Menucat.jsx
+++ src/pages/menu/Menucat.jsx
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import "./menumodal.css";
 
 const Menucat = () => {
-    // const containerStyling ={
-        
-    // }
 
   return (
 <div className='parentcontainer'>
     <div class="menucontainer">
-        <div class="categories">EATS MENU</div>
+        <div class="categories">
+          <a href="/Itemslist">EATS MENU</a></div>
         <div class="categories">DRINKS MENU</div>
         <div class="categories">SHISHA MENU</div>
         <div class="categories">MERCHANDISE</div>
